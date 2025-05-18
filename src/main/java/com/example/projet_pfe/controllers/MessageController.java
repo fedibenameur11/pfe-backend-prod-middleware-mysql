73c7fbbbef52460040e79a787377d69927ef38e0
Fedi Benameur <fedi.benameur@esprit.tn>
2025-05-18 12:46:25 +0100

    rabbitmq backend

--- src/main/java/com/example/projet_pfe/controllers/MessageController.java
+++ src/main/java/com/example/projet_pfe/controllers/MessageController.java
@@ -2,10 +2,14 @@ package com.example.projet_pfe.controllers;
 
 import com.example.projet_pfe.services.MessageProducer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/messages")
@@ -15,8 +19,11 @@ public class MessageController {
     private MessageProducer messageProducer;
 
     @PostMapping("/send")
-    public String sendMessage(@RequestBody String message) {
+    public ResponseEntity<Map<String, String>> sendMessage(@RequestBody String message) {
         messageProducer.sendMessage(message);
-        return "Message sent: " + message;
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("message", "Message sent: " + message);
+        return ResponseEntity.ok(response);
     }
 }
