649a8e9c0ef755168fe9a6d30fcefeada97b7633
Fedi Benameur <fedi.benameur@esprit.tn>
2025-05-21 10:27:52 +0100

    kafka docker-compose

--- src/main/java/com/example/projet_pfe/configuration/NatsConfig.java
+++ src/main/java/com/example/projet_pfe/configuration/NatsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class NatsConfig {
 
-    public static final String SUBJECT_NAME = "mySubject";
+    /*public static final String SUBJECT_NAME = "mySubject";
 
     @Bean
     public Connection natsConnection() throws Exception {
@@ -17,5 +17,5 @@ public class NatsConfig {
                 .server("nats://192.168.37.129:4222") // Remplacez par l'URL de votre serveur NATS
                 .build();
         return Nats.connect(options);
-    }
+    }*/
 }
